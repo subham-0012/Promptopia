ba52794b2b86de5f3cb147b0c34212f063f29850
subham <subhamgg162@gmail.com>
2024-06-23 19:35:41 +0530

    fix type

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
@@ -54,4 +54,4 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
