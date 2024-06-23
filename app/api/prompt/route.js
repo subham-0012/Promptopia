5877875ad16867ec02ef9eca7461b529c322b648
subham <subhamgg162@gmail.com>
2024-06-23 19:27:18 +0530

    initial commit

---
+++ app/api/prompt/route.js
@@ -0,0 +1,12 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+export const revalidate = 0;
+export const GET = async (request) => {
+    try {
+        await connectToDB();
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts),{status: 200})
+    } catch (error) {
+        return new Response(JSON.stringify("Failed to fetch all prompts"),{status: 500})
+    }
+}
